f0bfabb01c8961c57cd0818398a3493b5ab01038
beryltseng <56453474+beryltseng@users.noreply.github.com>
2020-01-03 15:12:00 -0700

    added state variable to Board

--- src/Board.js
+++ src/Board.js
@@ -2,40 +2,47 @@ import React from 'react';
 import Grid from './Grid.js';
 
 class Board extends React.Component {
+  state = {
+    board: [...Array(9).keys()].map(v => {
+        return [...Array(9).keys()].map(x => x+1);
+      })
+  }
+
   render() {
+    console.log(JSON.stringify(this.state.board));
     return (
       <div className="container">
         <div className="row">
           <div>
-            <Grid />
+            <Grid board={this.state.board} row={0} col={0}/>
           </div>
           <div>
-            <Grid />
+            <Grid board={this.state.board} row={0} col={1}/>
           </div>
           <div>
-            <Grid />
-          </div>
-        </div>
-        <div className="row">
-          <div>
-            <Grid />
-          </div>
-          <div>
-            <Grid />
-          </div>
-          <div>
-            <Grid />
+            <Grid board={this.state.board} row={0} col={2}/>
           </div>
         </div>
         <div className="row">
           <div>
-            <Grid />
+            <Grid board={this.state.board} row={1} col={0}/>
           </div>
           <div>
-            <Grid />
+            <Grid board={this.state.board} row={1} col={1}/>
           </div>
           <div>
-            <Grid />
+            <Grid board={this.state.board} row={1} col={2}/>
+          </div>
+        </div>
+        <div className="row">
+          <div>
+            <Grid board={this.state.board} row={2} col={0}/>
+          </div>
+          <div>
+            <Grid board={this.state.board} row={2} col={1}/>
+          </div>
+          <div>
+            <Grid board={this.state.board} row={2} col={2}/>
           </div>
         </div>
       </div>
