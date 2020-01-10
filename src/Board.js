6e7b3bbceacc458bae1d41bffca277a055c2e813
bertseng <tseng.beryl@gmail.com>
2020-01-10 09:45:52 -0700

    use css grid to display the game board

--- src/Board.js
+++ src/Board.js
@@ -11,40 +11,16 @@ class Board extends React.Component {
   render() {
     console.log(JSON.stringify(this.state.board));
     return (
-      <div className="container">
-        <div className="row">
-          <div>
-            <Grid board={this.state.board} row={0} col={0}/>
-          </div>
-          <div>
-            <Grid board={this.state.board} row={0} col={1}/>
-          </div>
-          <div>
-            <Grid board={this.state.board} row={0} col={2}/>
-          </div>
-        </div>
-        <div className="row">
-          <div>
-            <Grid board={this.state.board} row={1} col={0}/>
-          </div>
-          <div>
-            <Grid board={this.state.board} row={1} col={1}/>
-          </div>
-          <div>
-            <Grid board={this.state.board} row={1} col={2}/>
-          </div>
-        </div>
-        <div className="row">
-          <div>
-            <Grid board={this.state.board} row={2} col={0}/>
-          </div>
-          <div>
-            <Grid board={this.state.board} row={2} col={1}/>
-          </div>
-          <div>
-            <Grid board={this.state.board} row={2} col={2}/>
-          </div>
-        </div>
+      <div className="board">
+        <Grid board={this.state.board} row={0} col={0}/>
+        <Grid board={this.state.board} row={0} col={1}/>
+        <Grid board={this.state.board} row={0} col={2}/>
+        <Grid board={this.state.board} row={1} col={0}/>
+        <Grid board={this.state.board} row={1} col={1}/>
+        <Grid board={this.state.board} row={1} col={2}/>
+        <Grid board={this.state.board} row={2} col={0}/>
+        <Grid board={this.state.board} row={2} col={1}/>
+        <Grid board={this.state.board} row={2} col={2}/>
       </div>
     );
   }
--- src/Grid.js
+++ src/Grid.js
@@ -5,21 +5,15 @@ class Grid extends React.Component {
   render() {
     return (
       <div className="grid">
-        <div className="board-row">
-          <Square value="1"/>
-          <Square value="2"/>
-          <Square value="3"/>
-        </div>
-        <div className="board-row">
-          <Square value="4"/>
-          <Square value="5"/>
-          <Square value="6"/>
-        </div>
-        <div className="board-row">
-          <Square value="7"/>
-          <Square value="8"/>
-          <Square value="9"/>
-        </div>
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>
       </div>
     );
   }
